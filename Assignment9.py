e64fbc0e8f33a5fe38623d1e10f09d4741340033
saleemchisty <sksaleemchisty@gmail.com>
2021-05-12 14:59:59 +0530

    Update Assignment9.py

--- Assignment9.py
+++ Assignment9.py
@@ -1,25 +1,30 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # 1.To what does a relative path refer?
+1.To what does a relative path refer?
 A. A relative path refers to a location that is relative to a current directory. Relative paths make use of two special symbols, a dot (.) and a double-dot (..), which translate into the current directory and the parent directory
-# # 2. What does an absolute path start with your operating system?
+
+2. What does an absolute path start with your operating system?
 A.An absolute path begins with a drive letter followed by a colon, such as / or C:/
-# # 3.What do the functions os.getcwd() and os.chdir() do?
+
+3.What do the functions os.getcwd() and os.chdir() do?
 A.os.getcwd() method tells us the location of current working directory (CWD).chdir() method in Python used to change the current working directory to specified path. 
-# # 4.What are the . and .. folders?
+
+4.What are the . and .. folders?
 A. The . folder is the current folder, and .. is the parent folder.
-# # 5.In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+
+5.In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
 A. C:\bacon\eggs is the dir name, while spam.txt is the base name.
-# # 6.What are the three “mode” arguments that can be passed to the open() function?
+
+6.What are the three “mode” arguments that can be passed to the open() function?
 A. The string 'r' for read mode, 'w' for write mode, and 'a' for append mode
-# # 7.What happens if an existing file is opened in write mode?
+
+7.What happens if an existing file is opened in write mode?
 A.An existing file opened in write mode is erased and completely overwritten.
-# # 8.What is the difference between the read() and readlines() methods
+
+8.What is the difference between the read() and readlines() methods
 A.The read() method returns the file's entire contents as a single string value. The readlines() method returns a list of strings, where each string is a line from the file's contents.
-# # 9.What data structure does a shelf value resemble?
+
+9.What data structure does a shelf value resemble?
 A.A shelf value resembles a dictionary value; it has keys and values, along with keys() and values() methods that work similarly to the dictionary methods of the same names
-# In[ ]:
+
 
 
 
