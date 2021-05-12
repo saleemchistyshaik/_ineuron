85188e57fb64f54e5db4fbae94c91febc3a2b953
saleemchisty <sksaleemchisty@gmail.com>
2021-05-12 15:51:28 +0530

    Update Assignment_10.py

--- Assignment_10.py
+++ Assignment_10.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # 1.How do you distinguish between shutil.copy() and shutil.copytree()?
-
-# In[ ]:
-
-
+1.How do you distinguish between shutil.copy() and shutil.copytree()?
 A.shutil.copytree() method recursively copies an entire directory tree rooted at source (src) to the destination directory. The destination directory, named by (dst) must not already exist. 
 It will be created during copying. Permissions and times of directories are copied with copystat() and individual files are copied using shutil.copy2().
 
@@ -46,11 +39,11 @@ print("File Permission mode:", perm)
 print("Destination path:", dest)
 
 
-# # 2.What function is used to rename files?
+2.What function is used to rename files?
 A.os. rename() method in Python is used to rename a file or directory. This method renames a source file/ directory to specified destination file/directory
-# # 3.What is the difference between the delete functions in the send2trash and shutil modules?
+3.What is the difference between the delete functions in the send2trash and shutil modules?
 A.The send2trash functions will move a file or folder to the recycle bin, while shutil modules will permanently delete files and folders
-# # 4.ZipFile objects have a close() method just like File objects’ close() method. What ZipFile method is equivalent to File objects’ open() method?
+4.ZipFile objects have a close() method just like File objects’ close() method. What ZipFile method is equivalent to File objects’ open() method?
 A. ZipFile objects are conceptually similar to the File objects you saw returned by the open() function, they are values through which the program interacts with the file. To create a ZipFile object, call the zipfile.ZipFile() function.
 passing it a string of the .ZIP file’s filename. Note that zipfile is the name of the Python module, and ZipFile() is the name of the function.
 # In[ ]:
@@ -77,7 +70,7 @@ spamInfo.compress_size
 exampleZip.close()
 
 
-# # 5.Create a programme that searches a folder tree for files with a certain file extension (such as .pdf or .jpg). Copy these files from whatever location they are in to a new folder.
+5.Create a programme that searches a folder tree for files with a certain file extension (such as .pdf or .jpg). Copy these files from whatever location they are in to a new folder.
 
 # In[6]:
 
